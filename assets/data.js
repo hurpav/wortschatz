72d392abe9204ce97c3abc16384f274bcac564b5
hurpav <pavel.hurt81@gmail.com>
2026-06-17 11:48:41 +0200

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,1 +1,10 @@
-
+// assets/data.js
+async function loadData(filename) {
+    const url = `https://raw.githubusercontent.com/hurpav/wortschatz/main/${filename}?_ts=${new Date().getTime()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    // Automatické hledání pole slovíček v JSONu
+    if (Array.isArray(data)) return data;
+    const foundKey = Object.keys(data).find(key => Array.isArray(data[key]));
+    return foundKey ? data[foundKey] : [];
